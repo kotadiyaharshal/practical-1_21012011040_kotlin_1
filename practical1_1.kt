1ddf1c6c8d88c05284731e5712bf5765e5ec8bbd
student <student@gnu.ac.in>
2023-07-17 08:26:48 +0530

    practical-1_21012011040_kotlin_1.iml

---
+++ practical1_1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var a:Int = 22
+    var b:Float = 2.2F
+    var c:Double = 96.36
+    var d:String = "HMS"
+    var e:Char = 'V'
+
+    var f:Double = a.toDouble()
+    println("$f:Int to Double")
+
+    var g:Int = b.toInt()
+    println("$g:Float to Int")
+
+    var m:String = c.toString()
+    println("$m:Double to string")
+
+    var v:String = e.toString()
+    println("$v:Char to String")
+
+}
